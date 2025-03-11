4476656934ce428d1ff6ad08021af20b136a7abc
boben195 <boben195@ukr.net>
2025-03-11 21:31:35 +0200

    add first page and style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Card from "./pages/Card/Card";
+import Checkouts from "./pages/Checkouts/Checkouts";
+import Order from "./pages/Order/Order";
+import Footer from "./components/Footer/Footer";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>BOBOBOOB</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/card" element={<Card />} />
+        <Route path="/checkout" element={<Checkouts />} />
+        <Route path="/order" element={<Order />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { BiUser, BiCart } from "react-icons/bi";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <nav className="navbar">
+        <div>
+          <Link to="/">
+            <h2>FoodSpot</h2>
+          </Link>
+        </div>
+        <div className="search-bar">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search for products..."
+          />
+          <button>SEARCH</button>
+        </div>
+        <div className="icons">
+          <div>
+            <BiUser className="icon" />
+            <div className="dropdoun-menu">
+              <Link to="/login">
+                <p>Login/Sign Up</p>
+              </Link>
+              <Link to="/order">
+                <p>Orders</p>
+              </Link>
+              <p>Logout</p>
+            </div>
+          </div>
+          <button className="card-icon">
+            <BiCart className="icon" />
+            <span>0</span>
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
