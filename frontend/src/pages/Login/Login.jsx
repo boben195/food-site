301b9fbb0bcff35f0101f8d49c02c68c9a793f6d
boben195 <boben195@ukr.net>
2025-03-16 12:22:28 +0200

    add login page

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [currentState, serCurrentState] = useState("Login");
+  return (
+    <div>
+      <form className="auth-form" action="">
+        <div className="form-header">
+          <p className="form-title">{currentState}</p>
+        </div>
+
+        {currentState === "Login" ? null : (
+          <input
+            type="text"
+            className="form-input"
+            placeholder="Full name"
+            required
+          />
+        )}
+        <input
+          type="email"
+          className="form-input"
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          className="form-input"
+          placeholder="Password"
+          required
+        />
+        <div>
+          <p>Forget password ?</p>
+          {currentState === "Login" ? (
+            <p
+              className="toggle-auth-state"
+              onClick={() => serCurrentState("Sign Up")}
+            >
+              Create Account
+            </p>
+          ) : (
+            <p
+              className="toggle-auth-state"
+              onClick={() => serCurrentState("Login")}
+            >
+              Login Here
+            </p>
+          )}
+        </div>
+        <button className="form-btn">
+          {currentState === "Login" ? "Sign In" : "Sign Up"}
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
